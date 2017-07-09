9f9c978add5f6231a1603c3620e9017a41941ec9
maximmaxim345 <mraznatovski@gmail.com>
2017-07-09 19:07:10 +0200

    bugfixes

--- __init__.py
+++ __init__.py
@@ -1,22 +1,3 @@
-#  ***** BEGIN GPL LICENSE BLOCK *****
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  The Original Code is Copyright (C) 2017 maximmaxim345
-#  All rights reserved.
-#
-#  ***** END GPL LICENSE BLOCK *****
 import bpy, sys, os, time, tempfile
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from splinter import Browser
